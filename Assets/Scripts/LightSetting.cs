cd88202d0cdd3ec39b0aacc035ad1a11ce7891bf
Romibarbosa <romy_jul@hotmail.com>
2019-10-05 12:23:01 -0300

    Testing lights

---
+++ Assets/Scripts/LightSetting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSetting : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject lightGameObject = new GameObject("The Light");
+
+        // Add the light component
+        Light lightComp = lightGameObject.AddComponent<Light>();
+
+        // Set color and position
+        lightComp.color = Color.blue;
+
+        lightComp.intensity = 15;
+    }
+
+    
+}
---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public float xMin, xMax, yMin, yMax;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y );
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
