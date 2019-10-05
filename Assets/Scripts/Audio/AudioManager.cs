a69ca6e142e510aca862877b04f400ef823099c6
Romibarbosa <romy_jul@hotmail.com>
2019-10-05 17:16:42 -0300

    Add Audio Manager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Audio_instance;
+    public AudioSource[] Song;
+    public float timer;
+    float timer2;
+    AudioSource currentSong;
+    AudioSource newSong;
+    bool activatetimers;
+    bool activatetimers2;
+    AudioSource oldSong;
+    private void Awake()
+    {
+        Audio_instance = this;
+    }
+
+    public void ChangeMusic(string color)
+    {
+        switch(color)
+        {
+            case "Blue": PlayTheSong(0); break;
+            //case "Red": NewSong(1); break;
+            //case "Green": NewSong(2);  break;
+            //case "Yellow": NewSong(3);  break;
+            case "Violet": PlayTheSong(1);  break;
+            //case "Orange": NewSong(5); break;
+        }
+    }
+    public void PlayTheSong(int index)
+    {
+        if (currentSong != null)
+        {
+            AnteriorCancion(currentSong);
+        }
+
+        currentSong = Song[index];
+        currentSong.Play();
+        currentSong.volume = 0.5f;
+        activatetimers = true;
+        //ChangeVolume();
+
+        //currentSong = Song[indexSong];
+    }
+
+    public void AnteriorCancion(AudioSource audio)
+    {
+        oldSong = audio;
+        oldSong.volume = 0.5f;
+    }
+
+
+    private void Start()
+    {
+        for (int i = 0; i < Song.Length; i++)
+        {
+            Song[i].volume = 0;
+        }
+        currentSong = null;
+
+
+    }
+
+    private void Update()
+    {
+        if (currentSong != null)
+        {
+            currentSong.volume = 0.5f;
+        }
+
+        if (activatetimers)
+        {
+
+            timer += Time.deltaTime * 45;
+            timer2 -= Time.deltaTime * 45;
+            ChangeVolume();
+
+            if (timer>= 50)
+            {
+                activatetimers = false;
+                timer = 0;
+                timer2 = 50;
+            }
+            
+        }
+
+    }
+
+    public void ChangeVolume()
+    {
+        float value = timer / 100;
+        float value2 = timer2 / 100;
+
+        currentSong.volume = value;
+        if (oldSong != null)
+        {
+            oldSong.volume = value2;
+        }
+       
+
+    }
+
+
+}
