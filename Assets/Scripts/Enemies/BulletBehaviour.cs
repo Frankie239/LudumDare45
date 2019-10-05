30fcb88bf659cfb0507b06e183f59878135ff33e
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2019-10-05 16:39:54 -0300

    En la escena enemy esta implenetado el comportamiento de un enemigo torreta

---
+++ Assets/Scripts/Enemies/BulletBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float speed = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        Destroy(gameObject, 4);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    public Transform player;
+    Vector3 playerDist;
+    public LayerMask whatIsPlayer;
+    public float followRange;
+    Collider2D playerCol;
+
+    public GameObject bullet;
+    public Transform shotPoint;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+   
+    void Update()
+    {
+        playerCol = Physics2D.OverlapCircle(transform.position, followRange, whatIsPlayer);
+        playerDist = player.transform.position - transform.position;
+        LookAtAndFire();
+        Debug.Log(playerDist);
+    }
+
+    void LookAtAndFire()
+    {
+        if (playerCol != null) 
+        {
+            Quaternion rotation = Quaternion.LookRotation
+                (player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+            transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+            Fire();
+        }
+        else
+        {
+            Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    private void Fire()
+    {
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(bullet, shotPoint.position, transform.rotation);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, followRange);
+    }
+}
