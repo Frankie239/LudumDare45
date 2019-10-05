afed6f679d63fe204f2974f95788b4efc9abfeb7
Sefirost <fabriciocarella@hotmail.com>
2019-10-05 01:59:11 -0300

    change project to 2d

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(horizontal, vertical) * Time.deltaTime * movementSpeed;
+    }
+}
