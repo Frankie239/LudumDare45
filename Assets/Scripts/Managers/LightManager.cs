c6b4863050bbc09f925159c6e1bee090130a0cde
Romibarbosa <romy_jul@hotmail.com>
2019-10-07 12:44:43 -0300

    UI color lights

--- Assets/Scripts/Managers/LightManager.cs
+++ Assets/Scripts/Managers/LightManager.cs
@@ -21,9 +21,12 @@ public class LightManager : MonoBehaviour
 	private Light playerLight = default;
 	private GameObject endingLight = default;
 	private int activatedLights = 0;
+    SetLightUI lightUI;
 
 	void Awake()
 	{
+        lightUI = gameObject.GetComponent<SetLightUI>();
+
 		if (lightManager == null)
 		{
 			lightManager = this;
@@ -51,26 +54,32 @@ public class LightManager : MonoBehaviour
 			case Lights.Blue:
 				playerLight.color = new Color(0, 22, 255, 0);
                 AudioManager.Audio_instance.ChangeMusic("Blue");
+                lightUI.SetLight("blue");
 				break;
 			case Lights.Red:
 				playerLight.color = new Color(255, 22, 0, 0);
                 AudioManager.Audio_instance.ChangeMusic("Red");
+                lightUI.SetLight("red");
                 break;
 			case Lights.Green:
 				playerLight.color = new Color(29, 130, 24, 0);
                 AudioManager.Audio_instance.ChangeMusic("Green");
+                lightUI.SetLight("green");
                 break;
 			case Lights.Yellow:
 				playerLight.color = new Color(199, 201, 56, 0);
                 AudioManager.Audio_instance.ChangeMusic("Yellow");
+                lightUI.SetLight("yellow");
                 break;
 			case Lights.Violet:
 				playerLight.color = new Color(131, 59, 197, 0);
                 AudioManager.Audio_instance.ChangeMusic("Violet");
+                lightUI.SetLight("violet");
                 break;
 			case Lights.Orange:
 				playerLight.color = new Color(224, 144, 39, 0);
                 AudioManager.Audio_instance.ChangeMusic("Orange");
+                lightUI.SetLight("orange");
                 break;
 		}
 
---
+++ Assets/Scripts/UI/SetLightUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SetLightUI : MonoBehaviour
+{
+    public GameObject[] lights;
+    public Sprite[] colors;
+    int index;
+
+    private void Start()
+    {
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i].SetActive(false);
+        }
+    }
+//set color accede al elemento sprite y cambiale
+    public void SetLight(string color)
+    {
+        lights[index].SetActive(true);
+        lights[index].GetComponent<Image>().sprite = GetColor(color) ;
+        
+            index++;
+    }
+
+    Sprite GetColor(string color)
+    {
+        Sprite s = null;
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (colors[i].name == color)
+            {
+                s = colors[i];
+                Debug.Log("fiundcolor");
+            }
+        }
+
+        return s;
+    }
+
+}
